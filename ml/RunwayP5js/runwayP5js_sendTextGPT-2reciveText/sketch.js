84b2639a36334bd11bc085e21f7ede2f94338a97
AndreasRef <andreasrefsgaard@hotmail.com>
2019-03-22 13:52:41 +0100

    Added GPT-2 example

---
+++ ml/RunwayP5js/runwayP5js_sendTextGPT-2reciveText/sketch.js
@@ -0,0 +1,42 @@
+let input, button;
+
+let outputText = "";
+
+function setup() {
+    createCanvas(600,400);
+
+    input = createInput();
+    input.position(10, 10);
+
+    button = createButton('submit');
+    button.position(input.x + input.width, 10);
+    button.mousePressed(sendStringToRunway);
+}
+
+function draw() {
+    background(220);
+    text(outputText, 10, 30, width-10, height-10);
+}
+
+function sendStringToRunway() {
+    const inputs = {
+        "prompt": input.value()
+    };
+
+    fetch('http://localhost:8000/query', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(inputs)
+    })
+    .then(response => response.json())
+    .then(output => {
+    const { text } = output;
+    // use the outputs in your project
+    console.log(output);
+    outputText = output.text;
+  })
+}
+
