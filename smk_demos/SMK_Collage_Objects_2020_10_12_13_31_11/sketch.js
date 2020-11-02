d50da84638dfa701a43779630c3dcf63000bb6f3
Andreas Refsgaard <AndreasRef@users.noreply.github.com>
2020-11-02 14:14:30 +0100

    Nyborg

--- smk_demos/SMK_Collage_Objects_2020_10_12_13_31_11/sketch.js
+++ smk_demos/SMK_Collage_Objects_2020_10_12_13_31_11/sketch.js
@@ -1,5 +1,6 @@
-// to do: Remake as a dict like
-
+/* To do: 
+- Find bedre billeder
+*/
 let objectDetector;
 let input;
 
@@ -9,18 +10,23 @@ let status;
 let collageButton;
 
 let backgrounds = [];
-let nBackground = 2;
+let nBackground = 5;
 
 let apple, banana, bear, bed, boat, book, bottle, bowl, car, cat, chair, couch, cow, cup, dining_table, dog, elephant, fork, glas, horse, knife, orange, person, potted_plant, sheep, spoon, vase, zebra;
 
-let dict = {
-  "person": "person",
-  "cat": "cat",
-  "dog": "dog",
-};
+let detections;
 
+let nTestImages = 10;
+let testImages = [];
+
+let sel;
 
 function preload(){
+
+  for (let i = 0; i < nTestImages; i++) {
+    testImages[i] = loadImage('images/test' + i + '.jpg')
+  }
+
   img = loadImage('images/cat2.JPG');  
 
   apple = loadImage("images/apple.png")
@@ -48,7 +54,6 @@ function preload(){
   horse = loadImage("images/horse.png")
   knife = loadImage("images/knife.png")
 
-
   orange = loadImage("images/orange.png")
   person = loadImage("images/person.png")
   potted_plant = loadImage("images/potted_plant.png")
@@ -64,9 +69,19 @@ function preload(){
 
 function setup() {
   createCanvas(640, 420);
+  let lineBreak0 = createDiv("Try one of the pre-loaded images ");
+  sel = createSelect();
 
-  let lineBreak = createDiv("");
+  for (let i = 0; i < nTestImages; i++) {
+    testImages[i].resize(width, height);
+    sel.option(i)
+  }
+  sel.changed(mySelectEvent);
 
+
+  img = testImages[5];
+
+  let lineBreak1 = createDiv("<br> Or upload your own");
   input = createFileInput(handleFile);
   collageButton = createButton('collage');
   collageButton.mousePressed(collage);
@@ -77,7 +92,7 @@ function setup() {
 // Change the status when the model loads
 function modelReady() {
   console.log("model Ready!")
-  select('#modelStatus').html('Done!');
+  select('#modelStatus').html('Done! <br>This simple demo only works with the following objects:<br><em>apple, banana, bear, bed, boat, book, bottle, bowl, car, cat, chair, couch, cow, cup, dining_table, dog, elephant, fork, glas, horse, knife, orange, person, potted_plant, sheep, spoon, vase, zebra</em> <br><br>The backgrounds change randomly for now.');
   status = true;
   console.log('Detecting') 
   objectDetector.detect(img, gotResult);
@@ -89,7 +104,7 @@ function gotResult(err, results) {
     console.log(err);
   }
   console.log(results)
-  objects = results;
+  detections = results;
   drawStuff();
 }
 
@@ -99,41 +114,23 @@ function drawStuff() {
   if (status != undefined) {
     image(img, 0, 0)
 
-    for (let i = 0; i < objects.length; i++) {
+    for (let i = 0; i < detections.length; i++) {
       noStroke();
       fill(0, 255, 0);
-      text(objects[i].label + " " + nfc(objects[i].confidence * 100.0, 2) + "%", objects[i].x + 5, objects[i].y + 15);
+      text(detections[i].label + " " + nfc(detections[i].confidence * 100.0, 2) + "%", detections[i].x + 5, detections[i].y + 15);
       noFill();
       strokeWeight(4);
       stroke(0, 255, 0);
-      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      rect(detections[i].x, detections[i].y, detections[i].width, detections[i].height);
     }
   }
 }
 
 function collage() {
-  image(backgrounds[0], 0, 0, width, height);
-
-  
-  
-
-  for (let i = 0; i < objects.length; i++) {
-  }
-
-  detections = objects;
-
+  image(backgrounds[floor(random(backgrounds.length))], 0, 0, width, height);
   detections.forEach(detection => {
-    // Map the size of the emoji to the size of the detected object
-    //textSize(max(detection.width, detection.height));
 
-    //console.log(detection.label);
-    console.log(dict[detection.label]);
-
-
-    //Sloppy manual
-    //let apple, banana, bear, bed, boat, book, bottle, bowl, car, cat, chair, couch, cow, cup, dining_table, dog, elephant, fork, 
-    //glas, horse, knife, orange, person, potted_plant, sheep, spoon, vase, zebra;
-
+    //Sloppy manual if-else hell :-D
     if (detection.label === "apple") {
       image(apple, detection.x, detection.y, detection.width, detection.height);
     } else if (detection.label === "banana") {
@@ -174,11 +171,7 @@ function collage() {
       image(glas, detection.x, detection.y, detection.width, detection.height);
     } else if (detection.label === "horse") {
       image(horse, detection.x, detection.y, detection.width, detection.height);
-    } 
-    
-    //glas, horse, knife, orange, person, potted_plant, sheep, spoon, vase, zebra;
-    
-    else if (detection.label === "knife") {
+    } else if (detection.label === "knife") {
       image(knife, detection.x, detection.y, detection.width, detection.height);
     } else if (detection.label === "orange") {
       image(orange, detection.x, detection.y, detection.width, detection.height);
@@ -195,7 +188,8 @@ function collage() {
     } else if (detection.label === "zebra") {
       image(zebra, detection.x, detection.y, detection.width, detection.height);
     } else {
-      console.log("sorry, no corresponding image...")
+      //text("sorry, no  image for" + detection.label + " in this demo",  detection.x, detection.y);
+      console.log("sorry, no corresponding image for " + detection.label)
     }
 
 
@@ -220,4 +214,14 @@ function userImageLoaded() {
   
   console.log('Detecting') 
   objectDetector.detect(img, gotResult);
+}
+
+function mySelectEvent() {
+  let item = sel.value();
+  console.log(item);
+  
+
+  img = testImages[item];
+  userImageLoaded();
+  
 }
