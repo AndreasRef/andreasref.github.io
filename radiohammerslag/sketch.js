f357263c12eb7ccfa8e6462c59595abd29c27fad
Andreas Refsgaard <AndreasRef@users.noreply.github.com>
2022-03-17 12:39:31 +0100

    Added backtrack

--- radiohammerslag/sketch.js
+++ radiohammerslag/sketch.js
@@ -1,18 +1,26 @@
 var soundclips = [];
 var button;
+var backtrackButton;
+var backtrack;
 
 function preload() {
+    backtrack = loadSound('backtrack.mp3');
     for (var i = 0; i<280; i++) {
         soundclips[i] = loadSound("mp3/" + i + ".mp3");
         console.log(i + "loaded");
     }
+    backtrack.loop();
 }
 
 function setup() {
     noCanvas();
-    button = createButton('start');
+    button = createButton('start Hammerslag');
     button.position(0, 0);
     button.mousePressed(afspil);
+    backtrackButton = createButton('start backtrack');
+    backtrackButton.position(150, 0);
+    backtrackButton.mousePressed(playBacktrack);
+
 }
 
 function draw() {
@@ -31,6 +39,10 @@ function afspil() {
 }
 
 function mousePressed() {
-    soundclips[0].play();
-    soundclips[0].onended(playNewClip);
+    // soundclips[0].play();
+    // soundclips[0].onended(playNewClip);
+}
+
+function playBacktrack() {
+    backtrack.play();
 }
